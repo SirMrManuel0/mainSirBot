7ca9783579ff45e664c73ad21817ac14220d5e03
SirMrManuel0 <139504269+SirMrManuel0@users.noreply.github.com>
2023-07-18 17:23:02 +0200

    added AppCommand.py & /ping

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import discord
+from discord import app_commands
 from discord.ext import commands
 from credentials import apiToken
 
-intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix='>', intents=intents)
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+bot = commands.Bot(command_prefix="$", intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    await bot.tree.sync()
+
+@bot.tree.command(name="ping", description="Sends the bot's latency in ms.")
+async def ping(interaction: discord.Interaction):
+    await interaction.response.send_message(f"Pong! {round(bot.latency) * 1000}ms.")
+
+
 
 bot.run(apiToken.discord_token)
