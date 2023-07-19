2400a32d29469b8f405d08826e6b900279885489
SirMrManuel0 <139504269+SirMrManuel0@users.noreply.github.com>
2023-07-19 11:36:36 +0200

    remember command name option added

--- cogs/RememberCommand.py
+++ cogs/RememberCommand.py
@@ -13,28 +13,56 @@ class RememberCommand(commands.Cog):
         print(f"{__name__} loaded successfully!")
 
     @app_commands.command(name="remember", description="give the bot something to remember")
-    async def remember(self, interaction: discord.Interaction, to_remember: str):
+    async def remember(self, interaction: discord.Interaction, to_remember: str, name: str=None):
         user_id = str(interaction.user.id)
 
-        if not os.path.exists("discord_storage_not_encrypted/" + user_id + ".txt"):
-            with open("discord_storage_not_encrypted/" + user_id + ".txt", "w") as file:
-                file.write("")
-        file = open("discord_storage_not_encrypted/" + user_id + ".txt", "r")
+        if not os.path.exists("discord_storage_not_encrypted/" + user_id + ".txt") and name is None:
+            with open(f"discord_storage_not_encrypted/{user_id}.txt", "w") as file:
+                file.write(to_remember)
+            await interaction.response.send_message(f"I have remembered: {to_remember}.")
+            return
+        elif not os.path.exists(f"discord_storage_not_encrypted/{user_id}.txt"):
+            with open(f"discord_storage_not_encrypted/{user_id}.txt", "w") as file:
+                file.write(f"\n{name}:\n" + to_remember)
+            await interaction.response.send_message(f"Under the name: {name}. I have remembered: {to_remember}.")
+            return
+        file = open(f"discord_storage_not_encrypted/{user_id}.txt", "r")
         lines = file.readlines()
         file.close()
-        if len(lines) == 0:
-            with open("discord_storage_not_encrypted/" + user_id + ".txt", "a") as f:
-                f.write(to_remember)
-            await interaction.response.send_message(f"I have remembered: {to_remember}")
-            return
-        if len(lines) > 0:
+        if len(lines) > 0 and name is None:
             lines[0] = to_remember
-            with open("discord_storage_not_encrypted/" + user_id + ".txt", "w") as f:
+            with open(f"discord_storage_not_encrypted/{user_id}.txt", "w") as f:
                 for line in lines:
                     f.write(line)
-            await interaction.response.send_message(f"I have remembered: {to_remember}")
+            await interaction.response.send_message(f"I have remembered: {to_remember}.")
             return
-
+        elif len(lines) > 0:
+            count = 0
+            for line in lines:
+                count += 1
+                if line == f"{name}:\n":
+                    break
+            if count % 2 != 0:
+                count = len(lines)
+            if count == len(lines) and not len(lines) == 1:
+                lines[1] = f"{name}:\n{to_remember}\n" + lines[1]
+                with open(f"discord_storage_not_encrypted/{user_id}.txt", "w") as file:
+                    for line in lines:
+                        file.write(line)
+                await interaction.response.send_message(f"Under the name: {name}. I have remembered: {to_remember}.")
+                return
+            elif count == len(lines) and len(lines) == 1:
+                with open(f"discord_storage_not_encrypted/{user_id}.txt", "a") as file:
+                    file.write(f"\n{name}:\n{to_remember}")
+                await interaction.response.send_message(f"Under the name: {name}. I have remembered: {to_remember}.")
+                return
+            if count < len(lines):
+                lines[count] = to_remember + "\n"
+                with open(f"discord_storage_not_encrypted/{user_id}.txt", "w") as file:
+                    for line in lines:
+                        file.write(line)
+                await interaction.response.send_message(f"Under the name: {name}. I have remembered: {to_remember}.")
+                return
 
 
 
