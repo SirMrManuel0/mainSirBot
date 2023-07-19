36757f3498e4fcb8bf894d3b645e3a4f1247b094
SirMrManuel0 <139504269+SirMrManuel0@users.noreply.github.com>
2023-07-19 18:20:31 +0200

    start of encryption segment for storage | keyMaking done

--- cogs/RememberCommand.py
+++ cogs/RememberCommand.py
@@ -2,6 +2,8 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 import os
+from credentials import keyMaker
+import en_decrypt
 
 
 class RememberCommand(commands.Cog):
@@ -15,24 +17,24 @@ class RememberCommand(commands.Cog):
 
     @app_commands.command(name="remember", description="give the bot something to remember")
     async def remember(self, interaction: discord.Interaction, to_remember: str, name: str = None):
-        user_id = str(interaction.user.id)
+        rem_id = str(interaction.user.id) + str(interaction.message.guild.id)
 
-        if not os.path.exists("discord_storage_not_encrypted/" + user_id + ".txt") and name is None:
-            with open(f"discord_storage_not_encrypted/{user_id}.txt", "w") as file:
+        if not os.path.exists("discord_storage_not_encrypted/" + rem_id + ".txt") and name is None:
+            with open(f"discord_storage_not_encrypted/{rem_id}.txt", "w") as file:
                 file.write(to_remember)
             await interaction.response.send_message(f"I have remembered: {to_remember}.")
             return
-        elif not os.path.exists(f"discord_storage_not_encrypted/{user_id}.txt"):
-            with open(f"discord_storage_not_encrypted/{user_id}.txt", "w") as file:
+        elif not os.path.exists(f"discord_storage_not_encrypted/{rem_id}.txt"):
+            with open(f"discord_storage_not_encrypted/{rem_id}.txt", "w") as file:
                 file.write(f"\n{name}:\n" + to_remember)
             await interaction.response.send_message(f"Under the name: {name}. I have remembered: {to_remember}.")
             return
-        file = open(f"discord_storage_not_encrypted/{user_id}.txt", "r")
+        file = open(f"discord_storage_not_encrypted/{rem_id}.txt", "r")
         lines = file.readlines()
         file.close()
         if len(lines) > 0 and name is None:
             lines[0] = to_remember
-            with open(f"discord_storage_not_encrypted/{user_id}.txt", "w") as f:
+            with open(f"discord_storage_not_encrypted/{rem_id}.txt", "w") as f:
                 for line in lines:
                     f.write(line)
             await interaction.response.send_message(f"I have remembered: {to_remember}.")
@@ -47,19 +49,19 @@ class RememberCommand(commands.Cog):
                 count = len(lines)
             if count == len(lines) and not len(lines) == 1:
                 lines[1] = f"{name}:\n{to_remember}\n" + lines[1]
-                with open(f"discord_storage_not_encrypted/{user_id}.txt", "w") as file:
+                with open(f"discord_storage_not_encrypted/{rem_id}.txt", "w") as file:
                     for line in lines:
                         file.write(line)
                 await interaction.response.send_message(f"Under the name: {name}. I have remembered: {to_remember}.")
                 return
             elif count == len(lines) and len(lines) == 1:
-                with open(f"discord_storage_not_encrypted/{user_id}.txt", "a") as file:
+                with open(f"discord_storage_not_encrypted/{rem_id}.txt", "a") as file:
                     file.write(f"\n{name}:\n{to_remember}")
                 await interaction.response.send_message(f"Under the name: {name}. I have remembered: {to_remember}.")
                 return
             if count < len(lines):
                 lines[count] = to_remember + "\n"
-                with open(f"discord_storage_not_encrypted/{user_id}.txt", "w") as file:
+                with open(f"discord_storage_not_encrypted/{rem_id}.txt", "w") as file:
                     for line in lines:
                         file.write(line)
                 await interaction.response.send_message(f"Under the name: {name}. I have remembered: {to_remember}.")
@@ -67,12 +69,12 @@ class RememberCommand(commands.Cog):
 
     @app_commands.command(name="recall", description="remember what the bot saved")
     async def recall(self, interaction: discord.Interaction, name: str = None):
-        user_id = str(interaction.user.id)
+        rec_id = str(interaction.user.id) + str(interaction.message.guild.id)
 
-        if not os.path.exists("discord_storage_not_encrypted/" + user_id + ".txt"):
+        if not os.path.exists("discord_storage_not_encrypted/" + rec_id + ".txt"):
             await interaction.response.send_message(f"I can not remember anything.")
             return
-        file = open("discord_storage_not_encrypted/" + user_id + ".txt", "r")
+        file = open("discord_storage_not_encrypted/" + rec_id + ".txt", "r")
         lines = file.readlines()
         file.close()
         if name is None and lines[0] == "":
---
+++ en_decrypt.py
@@ -0,0 +1,45 @@
+
+
+
+def encrypt(text, key):
+    arr_text = text_in_numbers(text)
+    ii = 0
+    for i in range(0, len(arr_text)):
+        if ii < (len(key) - 1):
+            arr_text[i] = int(arr_text[i]) + int(key[ii])
+        else:
+            ii = 0
+            arr_text[i] = int(arr_text[i]) + int(key[ii])
+        if int(arr_text[i]) >= 1114112:
+            arr_text[i] = int(arr_text[i]) - 1114112
+        ii += 1
+    arr_text = numbers_in_text(arr_text)
+    return str(arr_text)
+
+def numbers_in_text(numbers):
+    ret_numbers = ""
+    for i in numbers:
+        ret_numbers += chr(i)
+    return ret_numbers
+
+def text_in_numbers(text):
+    text_in_number = ""
+    for i in text:
+        text_in_number += ord(i)
+    return text_in_number.split('_')[:-1]
+
+
+def decrypt(text, key):
+    arr_text = text_in_numbers(text)
+    ii = 0
+    for i in range(0, len(arr_text)):
+        if ii < (len(key) - 1):
+            arr_text[i] = int(arr_text[i]) - int(key[ii])
+        else:
+            ii = 0
+            arr_text[i] = int(arr_text[i]) - int(key[ii])
+        if int(arr_text[i]) < 0:
+            arr_text[i] = int(arr_text[i]) + 1114112
+        ii += 1
+    arr_text = numbers_in_text(arr_text)
+    return str(arr_text)
