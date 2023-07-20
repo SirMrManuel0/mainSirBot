0cba4f4b82855fafd7e8440ee652e60822192a80
SirMrManuel0 <139504269+SirMrManuel0@users.noreply.github.com>
2023-07-20 19:25:20 +0200

    created server remember and recall command

--- cogs/RememberCommand.py
+++ cogs/RememberCommand.py
@@ -28,7 +28,9 @@ class RememberCommand(commands.Cog):
         if not os.path.exists(f"discord_storage/{str(guild_id)}/{str(user_id)}.txt") and name is None:
             # Case: something is to be remembered for the first time with no name
 
-            os.mkdir(f"discord_storage/{str(guild_id)}/")
+            if not os.path.exists(f"discord_storage/{str(guild_id)}/"):
+                os.mkdir(f"discord_storage/{str(guild_id)}/")
+
             with open(f"discord_storage/{str(guild_id)}/{str(user_id)}.txt", "w", encoding="utf-8") as file:
                 file.write(encrypted)
             await interaction.response.send_message(f"I have remembered: {to_remember}.")
@@ -36,7 +38,9 @@ class RememberCommand(commands.Cog):
         elif not os.path.exists(f"discord_storage/{str(guild_id)}/{str(user_id)}.txt"):
             # Case: something is to be remembered for the first time with name
 
-            os.mkdir(f"discord_storage/{str(guild_id)}/")
+            if not os.path.exists(f"discord_storage/{str(guild_id)}/"):
+                os.mkdir(f"discord_storage/{str(guild_id)}/")
+
             with open(f"discord_storage/{str(guild_id)}/{str(user_id)}.txt", "w", encoding="utf-8") as file:
                 file.write(f"\n{name}:\n" + encrypted)
             await interaction.response.send_message(f"Under the name: {name}. I have remembered: {to_remember}.")
@@ -47,7 +51,7 @@ class RememberCommand(commands.Cog):
         if len(lines) > 0 and name is None:
             # Case: something is to be remembered with no name
 
-            lines[0] = encrypted
+            lines[0] = encrypted + "\n"
             with open(f"discord_storage/{str(guild_id)}/{str(user_id)}.txt", "w", encoding="utf-8") as f:
                 for line in lines:
                     f.write(line)
@@ -61,7 +65,7 @@ class RememberCommand(commands.Cog):
                 count += 1
                 if line == f"{name}:\n":
                     break
-            if count % 2 != 0:  # the name must be on an even line if not then t was user written
+            if count % 2 != 0:  # the name must be on an even line if not then it was user written
                 count = len(lines)
 
             if count == len(lines) and not len(lines) == 1:
@@ -74,7 +78,7 @@ class RememberCommand(commands.Cog):
                 await interaction.response.send_message(f"Under the name: {name}. I have remembered: {to_remember}.")
                 return
             elif count == len(lines) and len(lines) == 1:
-                # Case: nothing to remember has yet been set
+                # Case: nothing to remember has yet been set with name
 
                 with open(f"discord_storage/{str(guild_id)}/{str(user_id)}.txt", "a", encoding="utf-8") as file:
                     file.write(f"\n{name}:\n{encrypted}")
@@ -116,7 +120,7 @@ class RememberCommand(commands.Cog):
         elif name is None:
             # Case: standard thing to remember
 
-            await interaction.response.send_message(f"I seem to remember: {en_decrypt.decrypt(lines[0], key)}.")
+            await interaction.response.send_message(f"I seem to remember: {en_decrypt.decrypt(lines[0], key)[:-1]}.")
             return
         if name is not None:
             # Case: something with a name will be recalled
@@ -131,7 +135,130 @@ class RememberCommand(commands.Cog):
 
                 await interaction.response.send_message(f"I can not remember something under the name: {name}.")
                 return
-            await interaction.response.send_message(f"Under the name: {name}. I seem to remember: {en_decrypt.decrypt(lines[count], key)}.")
+            await interaction.response.send_message(f"Under the name: {name}. I seem to remember: {en_decrypt.decrypt(lines[count], key)[:-1]}.")
+
+
+
+
+    @app_commands.command(name="server_remember", description="give the bot something to remember and everyone to recall")
+    async def server_remember(self, interaction: discord.Interaction, to_remember: str, name: str = None):
+        guild_id = interaction.guild_id
+
+        key = keyMaker.keyMakingServer(guild_id)
+
+        encrypted = en_decrypt.encrypt(to_remember, key)
+
+        if not os.path.exists(f"discord_storage/{str(guild_id)}/{str(guild_id)}.txt") and name is None:
+            # Case: something is to be remembered for the first time with no name
+
+            if not os.path.exists(f"discord_storage/{str(guild_id)}/"):
+                os.mkdir(f"discord_storage/{str(guild_id)}/")
+
+            with open(f"discord_storage/{str(guild_id)}/{str(guild_id)}.txt", "w", encoding="utf-8") as file:
+                file.write(encrypted)
+            await interaction.response.send_message(f"I have remembered: {to_remember}.")
+            return
+        elif not os.path.exists(f"discord_storage/{str(guild_id)}/{str(guild_id)}.txt"):
+            # Case: something is to be remembered for the first time with name
+
+            if not os.path.exists(f"discord_storage/{str(guild_id)}/"):
+                os.mkdir(f"discord_storage/{str(guild_id)}/")
+
+            with open(f"discord_storage/{str(guild_id)}/{str(guild_id)}.txt", "w", encoding="utf-8") as file:
+                file.write(f"\n{name}:\n" + encrypted)
+            await interaction.response.send_message(f"Under the name: {name}. I have remembered: {to_remember}.")
+            return
+        file = open(f"discord_storage/{str(guild_id)}/{str(guild_id)}.txt", "r", encoding="utf-8")
+        lines = file.readlines()
+        file.close()
+        if len(lines) > 0 and name is None:
+            # Case: something is to be remembered with no name
+
+            lines[0] = encrypted + "\n"
+            with open(f"discord_storage/{str(guild_id)}/{str(guild_id)}.txt", "w", encoding="utf-8") as f:
+                for line in lines:
+                    f.write(line)
+            await interaction.response.send_message(f"I have remembered: {to_remember}.")
+            return
+        elif len(lines) > 0:
+            # Case: something is to be remembered with name
+
+            count = 0
+            for line in lines:
+                count += 1
+                if line == f"{name}:\n":
+                    break
+            if count % 2 != 0:  # the name must be on an even line if not then it was user written
+                count = len(lines)
+
+            if count == len(lines) and not len(lines) == 1:
+                # Case: name is not used
+
+                lines[1] = f"{name}:\n{encrypted}\n" + lines[1]
+                with open(f"discord_storage/{str(guild_id)}/{str(guild_id)}.txt", "w", encoding="utf-8") as file:
+                    for line in lines:
+                        file.write(line)
+                await interaction.response.send_message(f"Under the name: {name}. I have remembered: {to_remember}.")
+                return
+            elif count == len(lines) and len(lines) == 1:
+                # Case: nothing to remember has yet been set with name
+
+                with open(f"discord_storage/{str(guild_id)}/{str(guild_id)}.txt", "a", encoding="utf-8") as file:
+                    file.write(f"\n{name}:\n{encrypted}")
+                await interaction.response.send_message(f"Under the name: {name}. I have remembered: {to_remember}.")
+                return
+            if count < len(lines):
+                # Case: name was already used
+
+                lines[count] = encrypted + "\n"
+                with open(f"discord_storage/{str(guild_id)}/{str(guild_id)}.txt", "w", encoding="utf-8") as file:
+                    for line in lines:
+                        file.write(line)
+                await interaction.response.send_message(f"Under the name: {name}. I have remembered: {to_remember}.")
+                return
+        await interaction.response.send_message(f"I can not remember: {to_remember}")
+
+
+
+    @app_commands.command(name="server_recall", description="remember what the bot saved for the server")
+    async def server_recall(self, interaction: discord.Interaction, name: str = None):
+        guild_id = interaction.guild_id
+
+        key = keyMaker.keyMakingServer(guild_id)
+
+        if not os.path.exists(f"discord_storage/{str(guild_id)}/{str(guild_id)}.txt"):
+            # Case: nothing to remember
+
+            await interaction.response.send_message(f"I can not remember anything.")
+            return
+        file = open(f"discord_storage/{str(guild_id)}/{str(guild_id)}.txt", "r", encoding="utf-8")
+        lines = file.readlines()
+        file.close()
+        if name is None and lines[0] == "":
+            # Case: file exists yet there is nothing to remember
+
+            await interaction.response.send_message(f"I can not remember anything. Maybe try using a name.")
+            return
+        elif name is None:
+            # Case: standard thing to remember
+
+            await interaction.response.send_message(f"I seem to remember: {en_decrypt.decrypt(lines[0], key)[:-1]}.")
+            return
+        if name is not None:
+            # Case: something with a name will be recalled
+
+            count = 0
+            for line in lines:
+                count += 1
+                if line == f"{name}:\n" and count % 2 == 0:
+                    break
+            if count == len(lines):
+                # Case: name does not exist
+
+                await interaction.response.send_message(f"I can not remember something under the name: {name}.")
+                return
+            await interaction.response.send_message(
+                f"Under the name: {name}. I seem to remember: {en_decrypt.decrypt(lines[count], key)[:-1]}.")
 
 
 async def setup(client):
